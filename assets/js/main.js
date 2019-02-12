fa9840134e3f2b82c7fed4ccc5abac4794462f3c
leanatan <nathooline@gmail.com>
2019-02-12 12:33:43 -0600

    Se quitaron espacios entre llaves

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
    cargarHeader();
-
+   cargarSlider();
+   cargarFooter();
    $("button").click(function(){
       $.ajax({url: "contenido/registro-usuario.html #registro-usuario", success: function(result){
         $("#contenido").html(result);
@@ -14,8 +15,15 @@ $(document).ready(function () {
        }});
     }
 
+    function cargarSlider(){
+      $.ajax({url: "contenido/slider.html #registro-usuario", success: function(result){
+         $("#header").html(result);
+       }});
+    }
 
-
-
-
+    function cargarFooter(){
+      $.ajax({url: "contenido/header.html #registro-usuario", success: function(result){
+         $("#header").html(result);
+       }});
+    }
    });
