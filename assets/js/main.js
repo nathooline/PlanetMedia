ba78ff1b989993a8b7889c47b383cc322abf5787
leanatan <nathooline@gmail.com>
2019-02-21 17:57:57 -0600

    Esta version maneja ajax desde el menú con una etiqueta <a>

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,18 @@ $(document).ready(function () {
    cargarHeader();
    cargarSlider();
    cargarFooter();
+   cargarContenido();
    $("button").click(function(){
       $.ajax({url: "contenido/registro-usuario.html #registro-usuario", success: function(result){
         $("#contenido").html(result);
       }});
     });
+
+    $('body').on('submit','#registro-usuario',function(e){
+      e.preventDefault();
+      alert("Estas a punto de enviar un formulario");
+    });
+    
 
     function cargarHeader(){
       $.ajax({url: "contenido/header.html #registro-usuario", success: function(result){
@@ -17,13 +24,30 @@ $(document).ready(function () {
 
     function cargarSlider(){
       $.ajax({url: "contenido/slider.html #registro-usuario", success: function(result){
-         $("#header").html(result);
+         $("#slider").html(result);
+       }});
+
+
+       $('body').on('#contenido2').click(function(e){
+         e.preventDefault();
+        $.ajax({url: "contenido/contenido2.html #registro-usuario", success: function(result){
+          $("#contenido").html(result);
+        }});
+      });
+  
+    }
+
+    function cargarContenido(){
+      $.ajax({url: "contenido/contenido.html #registro-usuario", success: function(result){
+         $("#contenido").html(result);
        }});
     }
 
     function cargarFooter(){
-      $.ajax({url: "contenido/header.html #registro-usuario", success: function(result){
-         $("#header").html(result);
+      $.ajax({url: "contenido/footer.html #registro-usuario", success: function(result){
+         $("#footer").html(result);
        }});
     }
+
+    
    });
